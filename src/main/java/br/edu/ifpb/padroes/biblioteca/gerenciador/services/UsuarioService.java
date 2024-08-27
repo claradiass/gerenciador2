3a662d7360fad11b5e908f3061b6f743f9f4566b
claradiass <claragoncalves370@gmail.com>
2024-08-27 09:05:31 -0300

    adicionando opção de deletar e atulizar usuário

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/services/UsuarioService.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/services/UsuarioService.java
@@ -3,6 +3,8 @@ package br.edu.ifpb.padroes.biblioteca.gerenciador.services;
 import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.UsuarioDTO;
 import br.edu.ifpb.padroes.biblioteca.gerenciador.models.Usuario;
 import br.edu.ifpb.padroes.biblioteca.gerenciador.repositories.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,20 @@ public class UsuarioService {
     public Usuario addUsuario(UsuarioDTO usuarioDTO) {
         Usuario novoUsuario = new Usuario(usuarioDTO);
         return repository.save(novoUsuario);
+    }
+
+    public Usuario deleteUser(Long id){
+        if(repository.existsById(id)){
+            return repository.delete(id);
+        } 
+        throw new EntityNotFoundException("Inexistente");
+    }
+
+    public Usuario putUser(Long id, UsuarioDTO usuarioDTO){
+        UsuarioDTO usuarioExistente = repository.findById(id).orElseThrow();
+        usuarioExistente.setNome(usuarioDTO.nome());
+        usuarioExistente.setEndereco(usuarioDTO.endereco());
+
+        return repository.save(usuarioExistente);
     }
 }
