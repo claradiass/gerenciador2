b6c7f04472d582454acbabb6158769f6ded79074
dinizhugo <hugodiniztech01@gmail.com>
2024-08-28 15:53:40 -0300

    feat: added feature login and register

---
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/controllers/AuthenticationController.java
@@ -0,0 +1,49 @@
+package br.edu.ifpb.padroes.biblioteca.gerenciador.controllers;
+
+import br.edu.ifpb.padroes.biblioteca.gerenciador.config.security.TokenService;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.AuthenticationDTO;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.UsuarioDTO;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.models.Usuario;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UsuarioRepository repository;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.cpf(), data.senha());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody UsuarioDTO data) {
+        if (this.repository.findByCpf(data.cpf()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.senha());
+        Usuario newUser = new Usuario(data.nome(), data.cpf(), encryptedPassword, data.dataNascimento(), data.endereco(), data.cargo());
+
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
