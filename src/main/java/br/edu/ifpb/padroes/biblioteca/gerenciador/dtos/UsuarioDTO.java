d55de0fc29383ecacc5a490b59b98ecc36db0cc7
dinizhugo <hugodiniztech01@gmail.com>
2024-08-28 15:57:30 -0300

    refactor: changed users model (param cargo)

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/UsuarioDTO.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.padroes.biblioteca.gerenciador.dtos;
 
+import br.edu.ifpb.padroes.biblioteca.gerenciador.models.UsuarioCargo;
+
 import java.util.Date;
 
-public record UsuarioDTO(String nome, String endereco, String cpf, Date dataNascimento, String senha, Long cargo) {}
+public record UsuarioDTO(String nome, String endereco, String cpf, Date dataNascimento, String senha, UsuarioCargo cargo) {}
--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/models/Usuario.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/models/Usuario.java
@@ -2,31 +2,46 @@ package br.edu.ifpb.padroes.biblioteca.gerenciador.models;
 
 import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.UsuarioDTO;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "usuario")
-public class Usuario implements Serializable {
+public class Usuario implements Serializable, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String endereco;
+    @Column(name = "cpf",unique = true)
     private String cpf;
     @Column(name = "data_nascimento")
     private Date dataNascimento;
     private String senha;
-    @Column(name = "id_cargo")
-    private Long cargo;
+    @Enumerated(EnumType.STRING)
+    private UsuarioCargo cargo;
 
     public Usuario() {
     }
 
-    public Usuario(Long id, String nome, String cpf, String senha, Date dataNascimento, String endereco, Long cargo) {
+    public Usuario(Long id, String nome, String cpf, String senha, Date dataNascimento, String endereco, UsuarioCargo cargo) {
         this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.cargo = cargo;
+    }
+
+    public Usuario(String nome, String cpf, String senha, Date dataNascimento, String endereco, UsuarioCargo cargo) {
         this.nome = nome;
         this.cpf = cpf;
         this.senha = senha;
@@ -92,12 +107,48 @@ public class Usuario implements Serializable {
         this.endereco = endereco;
     }
 
-    public Long getCargo() {
+    public UsuarioCargo getCargo() {
         return cargo;
     }
 
-    public void setCargo(Long cargo) {
+    public void setCargo(UsuarioCargo cargo) {
         this.cargo = cargo;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.cargo == UsuarioCargo.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.cpf;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
 
