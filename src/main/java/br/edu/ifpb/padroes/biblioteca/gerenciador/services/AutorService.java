fedc76f22ed61b0fe130d6429af5410a6c507ca7
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-08-30 11:33:34 -0300

    feat(AutorService): Criando a classe Service para Autor

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/services/AutorService.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/services/AutorService.java
@@ -1,2 +1,36 @@
-package br.edu.ifpb.padroes.biblioteca.gerenciador.services;public class AutorService {
+package br.edu.ifpb.padroes.biblioteca.gerenciador.services;
+
+import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.RequestAuthorDTO;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.models.Autor;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.repositories.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorService {
+    @Autowired
+    private AutorRepository repository;
+
+    public Autor insertAuthor(RequestAuthorDTO data) {
+        var author = findAuthorByName(data.nome());
+        if (author == null) {
+            return repository.save(new Autor(data));
+        }
+        throw new RuntimeException(""); // Subtituir por uma exceção personalizada
+    }
+
+    public Autor getAutorById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public Autor updateAuthor(Long id, RequestAuthorDTO data) {
+        var author = getAutorById(id);
+
+        author.setNome(data.nome());
+        return repository.save(author);
+    }
+
+    private Autor findAuthorByName(String name) {
+        return repository.findAutorByNome(name);
+    }
 }
