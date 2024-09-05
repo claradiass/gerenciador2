f838d72f6bebe106bef7861631d84d3a265b6f89
anielmelo <anielcontato@gmail.com>
2024-09-05 15:06:25 -0300

    feat: add edit and delete livro endpoints

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/controllers/LivroController.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/controllers/LivroController.java
@@ -15,7 +15,7 @@ public class LivroController {
     @Autowired
     private LivroService service;
 
-    @PostMapping
+    @PostMapping("/save")
     public ResponseEntity<Livro> createNewLivro(@RequestBody LivroDTO livroDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.insertLivro(livroDTO));
     }
@@ -24,5 +24,16 @@ public class LivroController {
     public ResponseEntity<Livro> getLivro(@PathVariable("id") Long id){
         var obj = service.getLivro(id);
         return ResponseEntity.ok(obj);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Livro> editLivro(@PathVariable("id") Long id, @RequestBody LivroDTO livroDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.updateLivro(id, livroDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteLivro(@PathVariable("id") Long id) {
+        service.deleteLivro(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
