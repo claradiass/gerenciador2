00ef80f099ff2221a7f17c53ff689e464f0a8293
anielmelo <anielcontato@gmail.com>
2024-09-09 16:32:39 -0300

    feat: add payment method

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/services/EmprestimoService.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/services/EmprestimoService.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.padroes.biblioteca.gerenciador.services;
 
 import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.EmprestimoDTO;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.RequestPagamentoDTO;
 import br.edu.ifpb.padroes.biblioteca.gerenciador.dtos.UpdateEmprestimoDTO;
 import br.edu.ifpb.padroes.biblioteca.gerenciador.models.Emprestimo;
 import br.edu.ifpb.padroes.biblioteca.gerenciador.models.Livro;
@@ -10,16 +11,21 @@ import br.edu.ifpb.padroes.biblioteca.gerenciador.validators.emprestimo.Empresti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class EmprestimoService {
 
     private final static double RATE = 1;
+
+    private final static Calendar calendar = Calendar.getInstance();
     @Autowired
     private EmprestimoRepository repository;
 
@@ -71,14 +77,16 @@ public class EmprestimoService {
         Emprestimo emprestimo = getEmprestimoById(id);
 
         if (dataDevolucao == null) {
-            throw new RuntimeException("Data de devolução é um requerida.");
+            throw new RuntimeException("Data de devolução é requerida.");
         }
 
         if (emprestimo.getDataDevolucao() != null) {
             throw new IllegalStateException("O livro já foi devolvido.");
-        } // fazer validador
+        }
 
-        double multa = this.calcularMulta(emprestimo, dataDevolucao);
+        if (!emprestimo.isPago()) {
+            throw new RuntimeException("O livro não pode ser devolvido.");
+        }
 
         emprestimo.setDataDevolucao(dataDevolucao);
 
@@ -88,12 +96,16 @@ public class EmprestimoService {
         return repository.save(emprestimo);
     }
 
-    public List<Emprestimo> getEmprestimoByUsuarioId(Long id) {
-        return repository.findByUsuarioId(id);
-    }
+    public void pagarMulta(RequestPagamentoDTO pagamentoDTO) {
+        Emprestimo emprestimo = repository.findByUsuarioAndLivro(pagamentoDTO.usuarioId(), pagamentoDTO.livroId()).orElseThrow(() -> new NoSuchElementException("Este empréstimo já foi pago!"));
 
-    public List<Emprestimo> getEmprestimoByLivroId(Long id) {
-        return repository.findByUsuarioId(id);
+        double multa = emprestimo.getMulta();
+
+        if (multa <= 0) { throw new RuntimeException("Empréstimo sem pendências."); }
+
+        emprestimo.setPago(true);
+
+        repository.save(emprestimo);
     }
 
     private void validarUsuarioELivroPorEmprestimoDTO(EmprestimoDTO emprestimoDTO) {
@@ -103,13 +115,32 @@ public class EmprestimoService {
     }
 
     private double calcularMulta(Emprestimo emprestimo, Date dataDevolucao) {
-        int days = (int) ChronoUnit.DAYS.between((Temporal) emprestimo.getDataEntregaPrevista(), (Temporal) dataDevolucao) + 1;
+        long dataPrevistaMillis = emprestimo.getDataEntregaPrevista().getTime();
+        long dataDevolucaoMillis = dataDevolucao.getTime();
 
-        if (days >= 1) {
-            return RATE * days;
+        long diferencaMillis = dataDevolucaoMillis - dataPrevistaMillis;
+
+        long diasDeAtraso = TimeUnit.DAYS.convert(diferencaMillis, TimeUnit.MILLISECONDS) + 1;
+
+        if (diasDeAtraso > 0) {
+            double multa = RATE * diasDeAtraso;
+
+            emprestimo.setMulta(multa);
+
+            repository.save(emprestimo);
+
+            return multa;
         }
 
         return 0;
+    }
+
+    public List<Emprestimo> getEmprestimoByUsuarioId(Long id) {
+        return repository.findByUsuarioId(id);
+    }
+
+    public List<Emprestimo> getEmprestimoByLivroId(Long id) {
+        return repository.findByUsuarioId(id);
     }
 }
 
