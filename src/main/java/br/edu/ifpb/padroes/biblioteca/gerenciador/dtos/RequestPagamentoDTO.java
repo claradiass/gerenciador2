5ae78fe398efba651cffb185ebbc6a9a09d80593
anielmelo <anielcontato@gmail.com>
2024-09-11 16:41:38 -0300

    fix: requests http

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/RequestPagamentoDTO.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/RequestPagamentoDTO.java
@@ -2,4 +2,4 @@ package br.edu.ifpb.padroes.biblioteca.gerenciador.dtos;
 
 import java.time.LocalDate;
 
-public record RequestPagamentoDTO(Long emprestimoId, LocalDate dataDevolucao) { }
+public record RequestPagamentoDTO(LocalDate dataDevolucao) { }
--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/ResquestDevolucaoDTO.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/ResquestDevolucaoDTO.java
@@ -4,7 +4,5 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-public record ResquestDevolucaoDTO(
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate dataDevolucao
-    ) {}
+public record ResquestDevolucaoDTO(LocalDate dataDevolucao) {}
 
