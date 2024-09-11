b9494593ab94a0c94d419c5db8b2a03816084d71
anielmelo <anielcontato@gmail.com>
2024-09-11 16:40:22 -0300

    fix: custom exceptions

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,10 +1,10 @@
 package br.edu.ifpb.padroes.biblioteca.gerenciador.controllers.exceptions;
 
-import br.edu.ifpb.padroes.biblioteca.gerenciador.services.exceptions.UsuarioNotFoundException;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.services.exceptions.AlreadyExistsException;
+import br.edu.ifpb.padroes.biblioteca.gerenciador.services.exceptions.NotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,10 +13,18 @@ import java.time.Instant;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(UsuarioNotFoundException.class)
-    public ResponseEntity<StandardError> usuarioNotFoundException(UsuarioNotFoundException e, HttpServletRequest request) {
-        String error = "User not Found";
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StandardError> genericNotFoundException(NotFoundException e, HttpServletRequest request) {
+        String error = "Object not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<StandardError> alreadyExistsException(AlreadyExistsException e, HttpServletRequest request) {
+        String error = "Object already exists";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
