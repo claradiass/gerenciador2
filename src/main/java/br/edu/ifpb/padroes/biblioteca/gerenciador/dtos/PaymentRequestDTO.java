f1bc56b6620af9a04b16bb143b7d4ceff4e05016
anielmelo <anielcontato@gmail.com>
2024-09-12 10:38:25 -0300

    fix: translate DTOs to english

--- src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/PaymentRequestDTO.java
+++ src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/dtos/PaymentRequestDTO.java
@@ -2,4 +2,4 @@ package br.edu.ifpb.padroes.biblioteca.gerenciador.dtos;
 
 import java.time.LocalDate;
 
-public record RequestPagamentoDTO(LocalDate dataDevolucao) { }
+public record PaymentRequestDTO(LocalDate dataDevolucao) { }
